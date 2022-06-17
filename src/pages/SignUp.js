d9ede688dd256e5f05a52693e7ae1b41e0bfd547
ge78huy <anar.akhmedov@tum.de>
2022-06-17 12:12:11 +0200

    fixed copyright links

--- src/pages/SignUp.js
+++ src/pages/SignUp.js
@@ -16,7 +16,7 @@ function Copyright(props) {
   return (
     <Typography variant="body2" color="text.secondary" align="center" {...props}>
       {'Copyright © '}
-      <Link color="inherit" href="https://mui.com/">
+      <Link color="inherit" href="https://github.com/NullRivalException">
         NullRivalException
       </Link>{' '}
       {new Date().getFullYear()}
